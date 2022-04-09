d539fecc5cbf72d202e29b6847803f0bb7421529
Luís Ventuzelos <luisventuzelos@gmail.com>
2022-04-09 17:26:22 +0100

    added file parsing

--- file-input.h
+++ file-input.h
@@ -1,13 +1,40 @@
-void readFile(const char *filePath)
+
+void readFile(const char *filePath, int *machines, int *jobs, int *operations)
 {
     FILE *file = fopen(filePath, "r");
     char line[256];
+    int i, j = 0;
 
     while (fgets(line, sizeof(line), file))
     {
-        if (line[0] != '#')
-            printf("%s", line);
+        if (i == 0)
+        {
+            *machines = atoi(line);
+        }
+        else if (i == 1)
+            *jobs = atoi(line);
+        else if (i == 2)
+            *operations = atoi(line);
+        else
+        {
+            int k = 0;
+            char *token = strtok(line, " ");
+            for (k = 0; k < *operations; k++)
+            {
+                printf("%s\n", token);
+                jobshop.jobs[j].operations[k].machineId = atoi(token);
+                token = strtok(NULL, " ");
+                printf("%s\n", token);
+                jobshop.jobs[j].operations[k].duration = atoi(token);
+                token = strtok(NULL, " ");
+            }
+            j++;
+        }
+
+        i++;
     }
+
+    printf("%d %d %d\n", *machines, *jobs, *operations);
 
     fclose(file);
 }
